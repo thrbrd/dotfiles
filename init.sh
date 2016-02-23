9e456acefdf67f490b651e52885f8a4e921bf94d
thrbrd <masatoshi@koeda.cc>
2016-02-23 18:39:46 +0900

    Update something

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.vimrc.plug ~/.vimrc.plug
 ln -s ~/dotfiles/.vimrc.general ~/.vimrc.general
 ln -s ~/dotfiles/.vimrc.plugin ~/.vimrc.plugin
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.agignore ~/.agignore
